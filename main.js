64df18646623c3ad53b34089d5cec498e974c2b4
Charly Reyes <carlos.reyesma@udlap.mx>
2020-10-27 01:24:14 -0600

    Primer Reto Terminado

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function paginaBienvenidos(){
+    window.location.href="../Proyecto-ATM/bienvenidos.html";
+}
+function paginaOpciones(){
+    window.location.href="../Proyecto-ATM/opciones.html";
+}
