8a45006afd47d5d45a24e48e4e4d1fe912e1e5fd
Daniel Calvillo <danielcalvillomancilla123@gmail.com>
2020-10-28 21:36:58 -0600

    Building the javascript logic

--- Views/Bienvenidos/bienvenidos.js
+++ Views/Bienvenidos/bienvenidos.js
@@ -1,5 +1,22 @@
-function paginaOpciones(){
-    window.location.href="../Opciones/opciones.html";
+var userNip = null;
+var validNip = 2435;
+var validador = false
+document.getElementById("inputNip").value = userNip;
+
+    function actualizarNip() {   
+        userNip = document.getElementById("inputNip").value;
+    }
+    // Renderizado condicional del botón continuar
+//bug de validación 
+function logUser(){
+    userNip = parseInt(userNip);
+    console.log(userNip)
+    if(userNip === validNip) {
+        alert("Logeado con éxito")
+        window.location.href="../Opciones/opciones.html";
+    } else if (!(userNip === validNip)){
+        alert("tu nip no es valido")
+    }
 }
 
 function paginaFinal(){
